ce00017d2d08fad09f14681e7c9b5fbade78f695
Juan José Gutiérrez de Quevedo Pérez <juanjo@gutierrezdequevedo.com>
2020-12-27 00:23:37 +0100

    day 19, part 2 and part 1 optimized

--- 19/2.py
+++ 19/2.py
@@ -8,7 +8,30 @@ def parse_rule(rule):
     return name, rules
 
 def validate_message(rules, rule, message, pos):
-    if rule[0][0].startswith("\""):
+#    print(f"validating rule {rule} on {message}:{pos}")
+    if rules['11'] == rule:
+        count = 0
+        retval = True
+        while retval:
+            retval, new_p = validate_message(rules, rules['42'], message, pos)
+            if retval:
+                pos = new_p
+                count += 1
+        if count < 2:
+            return False, pos
+        count2 = 0
+        retval = True
+        while retval:
+            retval, new_p = validate_message(rules, rules['31'], message, pos)
+            if retval:
+                pos = new_p
+                count2 += 1
+        if count2 < 1 or count2 >= count:
+            return False, pos
+        return True, pos
+    elif rule[0][0].startswith("\""):
+        if pos >= len(message):
+            return False, pos
         return rule[0][0][1] == message[pos], pos + 1
     elif len(rule) == 2:
         rv1, pos1 = validate_message(rules, [rule[0]], message, pos)
@@ -24,7 +47,7 @@ def validate_message(rules, rule, message, pos):
         for x in rule[0]:
             rv, new_p = validate_message(rules, rules[x], message, p)
             if not rv:
-                return False, pos
+                return False, new_p
             p = new_p
         return True, p
 
@@ -43,8 +66,9 @@ for c in content:
 
 retval = 0
 for m in messages:
-    rv, pos = validate_message(rules, rules['0'], m, 0)
+    rv, pos = validate_message(rules, rules['11'], m, 0)
     if rv and pos == len(m):
+        print(m)
         retval += 1
 
 print(retval)
